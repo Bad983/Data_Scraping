876e8056070dd1b4e510cc66a054f6c88803de24
Daniele Badiali <daniele.badiali@bper.it>
2022-08-02 18:38:49 +0200

    Inseriti dati per scaricare paradiso e purgatorio sia in versione originale che tradotta

--- data_scrapy/data_scrapy/spiders/data_dante.py
+++ data_scrapy/data_scrapy/spiders/data_dante.py
@@ -13,33 +13,40 @@ class DanteSpider(scrapy.Spider):
             'NAME': 'TRAD_1_FIORE',
             'TAG': 'td.t11.indenta',
             'PATH': '../../Opere/Dante/Traduzione/',
-            'OPEN_FILE_MODE': 'w'
-
+            'OPEN_FILE_MODE': 'w',
+            'MULTIPLE_PAGES': None,
+            'RE-PATTERN': None
         }
         self.TRAD_1_DETTO_AMORE = {
             'URL': 'https://www.danteonline.it/opere/index.php?opera=Detto%20d%27Amore%20-%20tr.%20Casciani-Kleinhenz',
             'NAME': 'TRAD_1_DETTO_AMORE',
             'TAG': 'td.t10.nonindentare',
             'PATH': '../../Opere/Dante/Traduzione/',
-            'OPEN_FILE_MODE': 'w'
+            'OPEN_FILE_MODE': 'w',
+            'MULTIPLE_PAGES': None,
+            'RE-PATTERN': None
         }
         self.TRAD_1_RIME = {
             'URL': 'https://www.danteonline.it/opere/index.php?opera=Dante%27s%20Lyric%20Poetry%20-%20tr.%20Foster-Boyde',
             'NAME': 'TRAD_1_RIME',
             'TAG': 'td.tprosa',
             'PATH': '../../Opere/Dante/Traduzione/',
-            'OPEN_FILE_MODE': 'w'
+            'OPEN_FILE_MODE': 'w',
+            'MULTIPLE_PAGES': None,
+            'RE-PATTERN': None
         }
         self.ORIG_CACCIA_DIANA = {
             'URL': 'http://boccaccio.letteraturaoperaomnia.org/boccaccio_caccia_di_diana.html',
             'NAME': 'ORIG_CACCIA_DIANA',
             'TAG': 'blockquote',
             'PATH': '../../Opere/Dante/Originale/',
-            'OPEN_FILE_MODE': 'w'
+            'OPEN_FILE_MODE': 'w',
+            'MULTIPLE_PAGES': None,
+            'RE-PATTERN': None
         }
-        self.ORIG_DIVINA_COMMEDIA = {
+        self.ORIG_DIVINA_COMMEDIA_INFERNO = {
             'URL': 'https://divinacommedia.weebly.com/inferno-canto-numpages.html',
-            'NAME': 'ORIG_DIVINA_COMMEDIA',
+            'NAME': 'ORIG_DIVINA_COMMEDIA_INFERNO',
             'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
                                'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
                                'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV'],
@@ -49,21 +56,65 @@ class DanteSpider(scrapy.Spider):
             'OPEN_FILE_MODE': 'a'
         }
 
-        self.TRAD_1_DIVINA_COMMEDIA = {
+        self.TRAD_2_DIVINA_COMMEDIA_INFERNO = {
             'URL': 'https://divinacommedia.weebly.com/inferno-canto-numpages.html',
-            'NAME': 'TRAD_1_DIVINA_COMMEDIA',
+            'NAME': 'TRAD_2_DIVINA_COMMEDIA_INFERNO',
             'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
                                'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
                                'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV'],
             'TAG': 'td.wsite-multicol-col div.paragraph em span',
             'PATH': '../../Opere/Dante/Traduzione/',
             'RE-PATTERN': r'[0-9]',
-            'OPEN_FILE_MODE': 'w'
+            'OPEN_FILE_MODE': 'a'
         }
 
-        # self.TRAD_1_CONVIVIO = 'https://www.danteonline.it/opere/index.php?opera=The%20Banquet%20-%20tr.%20Ryan'
-        # self.TRAD_2_CONVIVIO = 'https://www.danteonline.it/opere/index.php?opera=Dante%27s%20%22Il%20Convivio%22%20-%20tr.%20Lansing'
-        # self.path = "/Traduzioni/"
+        self.ORIG_DIVINA_COMMEDIA_PURGATORIO = {
+            'URL': 'https://divinacommedia.weebly.com/purgatorio-canto-numpages.html',
+            'NAME': 'ORIG_DIVINA_COMMEDIA_PURGATORIO',
+            'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
+                               'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
+                               'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII'],
+            'TAG': 'td.wsite-multicol-col div.paragraph',
+            'PATH': '../../Opere/Dante/Originale/',
+            'RE-PATTERN': r'[0-9]',
+            'OPEN_FILE_MODE': 'a'
+        }
+
+        self.TRAD_2_DIVINA_COMMEDIA_PURGATORIO = {
+            'URL': 'https://divinacommedia.weebly.com/purgatorio-canto-numpages.html',
+            'NAME': 'TRAD_2_DIVINA_COMMEDIA_PURGATORIO',
+            'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
+                               'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
+                               'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII'],
+            'TAG': 'td.wsite-multicol-col div.paragraph em span',
+            'PATH': '../../Opere/Dante/Traduzione/',
+            'RE-PATTERN': r'[0-9]',
+            'OPEN_FILE_MODE': 'a'
+        }
+
+        self.ORIG_DIVINA_COMMEDIA_PARADISO = {
+            'URL': 'https://divinacommedia.weebly.com/paradiso-canto-numpages.html',
+            'NAME': 'ORIG_DIVINA_COMMEDIA_PARADISO',
+            'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
+                               'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
+                               'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII'],
+            'TAG': 'td.wsite-multicol-col div.paragraph',
+            'PATH': '../../Opere/Dante/Originale/',
+            'RE-PATTERN': r'[0-9]',
+            'OPEN_FILE_MODE': 'a'
+        }
+
+        self.TRAD_2_DIVINA_COMMEDIA_PARADISO = {
+            'URL': 'https://divinacommedia.weebly.com/paradiso-canto-numpages.html',
+            'NAME': 'TRAD_2_DIVINA_COMMEDIA_PARADISO',
+            'MULTIPLE_PAGES': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
+                               'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI',
+                               'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII'],
+            'TAG': 'td.wsite-multicol-col div.paragraph em span',
+            'PATH': '../../Opere/Dante/Traduzione/',
+            'RE-PATTERN': r'[0-9]',
+            'OPEN_FILE_MODE': 'a'
+        }
 
     def start_requests(self):
         opere = [
@@ -71,8 +122,12 @@ class DanteSpider(scrapy.Spider):
             # self.TRAD_1_DETTO_AMORE,
             # self.TRAD_1_RIME,
             # self.ORIG_CACCIA_DIANA,
-            # self.ORIG_DIVINA_COMMEDIA,
-            self.TRAD_1_DIVINA_COMMEDIA
+            self.ORIG_DIVINA_COMMEDIA_INFERNO,
+            self.TRAD_2_DIVINA_COMMEDIA_INFERNO
+            self.ORIG_DIVINA_COMMEDIA_PURGATORIO,
+            self.TRAD_2_DIVINA_COMMEDIA_PURGATORIO
+            self.ORIG_DIVINA_COMMEDIA_PARADISO,
+            self.TRAD_2_DIVINA_COMMEDIA_PARADISO
             # self.TRAD_1_CONVIVIO,
             # self.TRAD_2_CONVIVIO
         ]
@@ -100,7 +155,7 @@ class DanteSpider(scrapy.Spider):
         with open(filename, open_file_mode) as f:
 
             for resp in response.css(tag + "::text").getall():
-                if pattern != "":
+                if pattern is not None:
                     resp = re.sub(pattern, '', resp)
 
                 f.write(resp)
